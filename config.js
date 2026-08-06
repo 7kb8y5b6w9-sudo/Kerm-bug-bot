2c6e89e40ca963bb7b1ea93fff64b63a19e66183
7kb8y5b6w9-sudo <7kb8y5b6w9@privaterelay.appleid.com>
2026-08-06 01:02:42 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 const chalk = require("chalk")
 const fs = require("fs")
 
-global.ownerNumber = ["237656520674@s.whatsapp.net"]
-global.nomerOwner = "237656520674"
-global.nomorOwner = ['237656520674']
-global.namaDeveloper = "𝐊𝐞𝐫𝐦" //jangn diubh bng
+global.ownerNumber = ["243995499574@s.whatsapp.net"]
+global.nomerOwner = "243995499574"
+global.nomorOwner = ['243995499574']
+global.namaDeveloper = "𝐂𝐚𝐩𝐩𝐮𝐜𝐜𝐢𝐧𝐨" //jangn diubh bng
 global.namaOwner = "𝐊𝐞𝐫𝐦"
 global.namaBot = "𝐊𝐞𝐫𝐦 𝐁𝐮𝐠 𝐕𝟔"
 global.versionBot = "𝟏𝟐.𝟎.𝟎"
